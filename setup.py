869eedb16f4edc5f2fc50d80623730a28a351adb
Andrea Stallvik <grimsdatter@gmail.com>
2023-08-29 12:54:17 +0200

    configuration

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='BIOTBT3900',
+    version='0.1',
+    packages=find_packages(),
+
+    # Metadata
+    author='Andrea Stallvik',
+    author_email='andregst@stud.ntnu.no',
+    description='Master project of Andrea Stallvik',
+    url='https://github.com/andreastallvik/BIOTBT3900',
+    
+    # Requirements
+    install_requires=[
+        #TODO: put in req. file ect.
+    ],
+)
