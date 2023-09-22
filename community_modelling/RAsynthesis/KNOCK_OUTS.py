47fa032978bccc6e4f536f9f5387544b8a8665a2
Andrea Stallvik <grimsdatter@gmail.com>
2023-09-22 15:10:49 +0200

    xylose/glucose triculture

---
+++ community_modelling/RAsynthesis/KNOCK_OUTS.py
@@ -0,0 +1,11 @@
+"""Need a place to store the knock-outs that need be performed, both for cobrapy and reframed."""
+
+CAL2_KO = ["b2599"]
+
+MRA_KO = ["b2599"]
+
+CAL11_KO = ["b2599", "b2415", "b2416", "b2417", "b1692"]
+
+MAM3_KO = ["b2599", "b2415", "b2416", "b2417", "b1692", "b3281"]
+
+SAL11_KO = ["ECD_03417"]
--- community_modelling/RAsynthesis/create_GEMS.py
+++ community_modelling/RAsynthesis/create_GEMS.py
@@ -928,7 +928,516 @@ def mra():
     write_sbml_model(MRA, file_path)
 
 
+def cal11():
 
+    print("loading GEMs...")
+
+    print("... k12 model")
+    # read model for E. coli K12
+    k12 = read_sbml_model('../GEMs/iML1515.xml')
+
+    print("... universal model")
+    #read the universal model
+    uni = read_sbml_model(str(ROOT_DIR / "community_modelling" / "GEMs" / "bigg_universe.xml"))
+
+    CAL11 = k12.copy()
+
+    ############### Knock-outs ###############
+
+    # pheA | gene b2599
+    knock_out_model_genes(CAL11, ["b2599"])
+    # ptsH | b2415
+    knock_out_model_genes(CAL11, ["b2415"])
+    # ptsI | b2416
+    knock_out_model_genes(CAL11, ["b2416"])
+    # crr | b2417
+    knock_out_model_genes(CAL11, ["b2417"])
+    # ydiB | b1692
+    knock_out_model_genes(CAL11, ["b1692"])
+
+    #NOTE: might need to knock out more to fully disable glucose uptake / just set glucose transport to 0 or something
+
+    ############### Metabolites ###############
+
+    print("adding metabolites")
+
+    #p-Coumaric acid
+    p_coumaric_acid_c = uni.metabolites.get_by_id("T4hcinnm_c")
+    p_coumaric_acid_c.compartment = "c"
+
+    #caffeic acid
+    caffeic_acid_c = uni.metabolites.get_by_id("34dhcinm_c")
+    caffeic_acid_c.compartment = "c"
+
+    #external caffeic acid
+    caffeic_acid_e = uni.metabolites.get_by_id("34dhcinm_e")
+    caffeic_acid_e.compartment = "e"
+
+    #ammonia
+    nh4_c = k12.metabolites.get_by_id("nh4_c")
+
+    #tyrosine 
+    tyr__L_c = k12.metabolites.get_by_id("tyr__L_c")
+
+    #NADH
+    nadh_c = k12.metabolites.get_by_id("nadh_c")
+
+    #NAD+
+    nad_c = k12.metabolites.get_by_id("nad_c")
+
+    #H+
+    h_c = k12.metabolites.get_by_id("h_c")
+
+    #ATP
+    atp_c = k12.metabolites.get_by_id("atp_c")
+
+    #AMP
+    adp_c = k12.metabolites.get_by_id("adp_c")
+
+    #Diphosphate
+    pi_c = k12.metabolites.get_by_id("pi_c")
+
+    ############### Reactions ###############
+
+    print("adding reactions")
+
+    #tyr -> p-coumaric acid
+    TAL = Reaction('TAL')
+    TAL.name = 'L-tyrosine ammonia-lyase'
+    TAL.subsystem = 'CA module'
+    TAL.lower_bound = -1000  # This is the default
+    TAL.upper_bound = 1000  # This is the default
+    #L-Tyrosine <=> 4-Coumarate + Ammonia
+    TAL.add_metabolites({
+        tyr__L_c: -1.0,
+        p_coumaric_acid_c: 1.0,
+        nh4_c: 1.0
+    })
+    #TAL.gene_reaction_rule = 'tal' #synthetic rgTAL gene
+
+    #p-coumaric acid -> caffeic acid
+    COURCA = Reaction('COURCA')
+    COURCA.name = '4-hydroxyphenylacetate 3-hydroxylase'
+    COURCA.subsystem = 'CA module'
+    COURCA.lower_bound = -1000  # This is the default
+    COURCA.upper_bound = 1000  # This is the default
+    #4-Coumarate <=> Caffeate
+    COURCA.add_metabolites({
+        p_coumaric_acid_c: -1.0,
+        nadh_c: -1.0,
+        caffeic_acid_c: 1.0,
+        nad_c: 1.0,
+        h_c: 1.0
+    })
+    #COURCA.gene_reaction_rule = '(hpaB and hpaC)' #synthetic hpaC gene
+
+    #caffeic acid transport reaction
+    CAFFAt = Reaction('34DHCINMt')
+    CAFFAt.name = 'caffeic acid active transport'
+    CAFFAt.subsystem = 'CA module'
+    CAFFAt.lower_bound = -1000  # This is the default
+    CAFFAt.upper_bound = 1000  # This is the default
+    CAFFAt.add_metabolites({
+        caffeic_acid_c: -1.0,
+        atp_c: -1.0,
+        caffeic_acid_e: 1.0,
+        adp_c: 1.0,
+        pi_c: 1.0
+    })
+
+    # add reactions to model
+    CAL11.add_reactions([TAL, COURCA, CAFFAt])
+
+    #exhange reactions for CA
+    #CAL11.metabolites.get_by_id("34dhcinm_e").compartment = 'e' #set compartment to be 'e' rather than 'C_e'
+    CAL11.add_boundary(CAL11.metabolites.get_by_id("34dhcinm_e"), type="exchange")
+    
+    #stopping caffeic acid uptake so that the model does not use this as substrate
+    CAL11.reactions.EX_34dhcinm_e.lower_bound = 0
+
+    # allow uptake of xylose
+    CAL11.reactions.EX_xyl__D_e.lower_bound = -10
+    
+    print("Testing model")
+
+    # # run tests to see that the model functions as it should
+    assert no_energy_gen_cycles(CAL11), "Energy generation cycle detected"
+    assert production_possible(CAL11, "EX_34dhcinm_e"), "CA production not possible"
+
+    #updtate the model ID
+    CAL11.id = 'CAL11'
+
+    # write model to file
+    file_path = ROOT_DIR / "community_modelling" / "GEMs" / "CAL11.xml"
+    print("Writing model to file as", str(file_path))
+    write_sbml_model(CAL11, file_path)
+
+
+def mam3():
+
+    print("loading GEMs...")
+
+    print("... k12 model")
+    # read model for E. coli K12
+    k12 = read_sbml_model(str(ROOT_DIR / "community_modelling" / "GEMs" / "iML1515.xml"))
+
+    print("... universal model")
+    #read the universal model
+    uni = read_sbml_model(str(ROOT_DIR / "community_modelling" / "GEMs" / "bigg_universe.xml"))
+
+    MAM3 = k12.copy()
+
+    ############### Knock-outs ###############
+
+    # pheA | gene b2599
+    knock_out_model_genes(MAM3, ["b2599"])
+    # ptsH | b2415
+    knock_out_model_genes(MAM3, ["b2415"])
+    # ptsI | b2416
+    knock_out_model_genes(MAM3, ["b2416"])
+    # crr | b2417
+    knock_out_model_genes(MAM3, ["b2417"])
+    # ydiB | b1692
+    knock_out_model_genes(MAM3, ["b1692"])
+    # aroE | b3281
+    knock_out_model_genes(MAM3, ["b3281"])
+
+    #NOTE: might need to knock out more to fully disable glucose uptake / just set glucose transport to 0 or something
+
+    ############### Metabolites ###############
+
+    print("adding metabolites")
+
+    #caffeic acid
+    caffeic_acid_c = uni.metabolites.get_by_id("34dhcinm_c")
+    caffeic_acid_c.compartment = 'c'
+
+    #external caffeic acid
+    caffeic_acid_e = uni.metabolites.get_by_id("34dhcinm_e")
+    caffeic_acid_e.compartment = 'e'
+
+    #caffeoyl CoA
+    caffcoa_c = uni.metabolites.get_by_id("caffcoa_c")
+    caffcoa_c.compartment = 'c'
+
+    #3,4-Dihydroxyphenyllactic acid | Salvianic acid A | SAA
+    saa_c = Metabolite(
+        'saa_c',
+        formula='C9H10O5',
+        name='3,4-Dihydroxyphenyllactic acid',
+        compartment='c')
+
+    #external 3,4-Dihydroxyphenyllactic acid | Salvianic acid A | SAA
+    saa_e = Metabolite(
+        'saa_e',
+        formula='C9H10O5',
+        name='3,4-Dihydroxyphenyllactic acid',
+        compartment='e')
+
+    #rosmarinic acid
+    rosma_c = Metabolite(
+        'rosma_c',
+        formula='C18H16O8',
+        name='Rosmarinic acid',
+        compartment='c')
+
+    #external rosmarinic acid
+    rosma_e = Metabolite(
+        'rosma_e',
+        formula='C18H16O8',
+        name='Rosmarinic acid',
+        compartment='e')
+
+    #ATP
+    atp_c = k12.metabolites.get_by_id("atp_c")
+
+    #AMP
+    amp_c = k12.metabolites.get_by_id("amp_c")
+
+    #Diphosphate
+    ppi_c = k12.metabolites.get_by_id("ppi_c")
+
+    #CoA
+    coa_c = k12.metabolites.get_by_id("coa_c")
+
+    ############### Reactions ###############
+
+    print("adding reactions")
+
+    #caffeic acid transport reaction
+    CAFFAt = Reaction('CAFFAt')
+    CAFFAt.name = 'caffa transport'
+    CAFFAt.subsystem = 'RA module'
+    CAFFAt.lower_bound = -1000  # This is the default
+    CAFFAt.upper_bound = 1000  # This is the default
+    CAFFAt.add_metabolites({
+        caffeic_acid_c: -1.0,
+        caffeic_acid_e: 1.0
+    })
+
+    CAFFCOA = uni.reactions.get_by_id("CAFFCOA")
+
+
+    #SAA transport reaction
+    SAAt = Reaction('SAAt')
+    SAAt.name = 'caffa transport'
+    SAAt.subsystem = 'RA module'
+    SAAt.lower_bound = -1000  # This is the default
+    SAAt.upper_bound = 1000  # This is the default
+    SAAt.add_metabolites({
+        saa_c: -1.0,
+        saa_e: 1.0
+    })
+
+
+    #salvianic acid A + caffeoyl CoA -> Rosmarinic acid
+    RAS = Reaction('RAS')
+    RAS.name = 'rosmarinic acid synthase'
+    RAS.subsystem = 'RA module'
+    RAS.lower_bound = -1000  # This is the default
+    RAS.upper_bound = 1000  # This is the default
+    #Caffeoyl-CoA + 3-(3,4-Dihydroxyphenyl)lactate <=> CoA + Rosmarinate
+    RAS.add_metabolites({
+        caffcoa_c: -1.0,
+        saa_c: -1.0,
+        coa_c: 1.0,
+        rosma_c: 1.0
+    })
+    #RAS.gene_reaction_rule = '(ras)' #synthetic 4CL gene
+
+
+    #RA transport reaction
+    RAt = Reaction('RAt')
+    RAt.name = 'caffa transport'
+    RAt.subsystem = 'RA module'
+    RAt.lower_bound = -1000  # This is the default
+    RAt.upper_bound = 1000  # This is the default
+    RAt.add_metabolites({
+        rosma_c: -1.0,
+        rosma_e: 1.0
+    })
+
+    # add reactions to model
+    MAM3.add_reactions([CAFFAt, CAFFCOA, SAAt, RAS, RAt])
+
+    #exhange reactions for SA, caffa, and RA
+
+    #MAM3.metabolites.get_by_id("34dhcinm_e").compartment = 'e' #set compartment to be 'e' rather than 'C_e'
+
+    MAM3.add_boundary(MAM3.metabolites.get_by_id("34dhcinm_e"), type="exchange")
+    MAM3.add_boundary(MAM3.metabolites.get_by_id("saa_e"), type="exchange")
+    MAM3.add_boundary(MAM3.metabolites.get_by_id("rosma_e"), type="exchange")
+
+    #so that cell cannot eat rosmarinic acid
+    MAM3.reactions.EX_rosma_e.lower_bound = 0
+
+    print("Testing model")
+
+    # # run tests to see that the model functions as it should
+    assert no_energy_gen_cycles(MAM3), "Energy generation cycle detected"
+    assert production_possible(MAM3, "EX_rosma_e"), "RA production not possible"
+
+    #updtate the model ID
+    MAM3.id = 'MAM3'
+
+    # write model to file
+    file_path = ROOT_DIR / "community_modelling" / "GEMs" / "MAM3.xml"
+    print("Writing model to file as", str(file_path))
+    write_sbml_model(MAM3, file_path)
+
+
+def sal11():
+
+    print("loading GEMs...")
+
+    print("... bl21 model")
+    # read model for E. coli BL21
+    bl21 = read_sbml_model('../GEMs/iHK1487.xml')
+
+    print("... universal model")
+    #read the universal model
+    uni = read_sbml_model(str(ROOT_DIR / "community_modelling" / "GEMs" / "bigg_universe.xml"))
+
+    SAL11 = bl21.copy()
+
+    ############### Knock-outs ###############
+
+    # xylA (b3565) | ECD_03417
+    knock_out_model_genes(SAL11, ["ECD_03417"])
+
+    ############### Metabolites ###############
+
+    print("adding metabolites")
+
+    #4-hydroxyphenyllactate
+    hpl34_c = uni.metabolites.get_by_id("34hpl_c")
+    hpl34_c.compartment = 'c'
+
+    #3,4-dihydroxyphenylpyruvate
+    dhpp34_c = Metabolite(
+        '34dhpp_c',
+        formula='C9H8O5',
+        name='3,4-Dihydroxyphenylpyruvate',
+        compartment='c')
+
+    #3,4-Dihydroxyphenyllactic acid | Salvianic acid A | SAA
+    saa_c = Metabolite(
+        'saa_c',
+        formula='C9H10O5',
+        name='3,4-Dihydroxyphenyllactic acid',
+        compartment='c')
+
+    #external 3,4-Dihydroxyphenyllactic acid | Salvianic acid A | SAA
+    saa_e = Metabolite(
+        'saa_e',
+        formula='C9H10O5',
+        name='3,4-Dihydroxyphenyllactic acid',
+        compartment='e')
+
+    #loading existing metabolites for bl21
+
+    #HPP
+    hpp34_c = bl21.metabolites.get_by_id("34hpp_c")
+
+    #NADH
+    nadh_c = bl21.metabolites.get_by_id("nadh_c")
+
+    #NAD+
+    nad_c = bl21.metabolites.get_by_id("nad_c")
+
+    #H+
+    h_c = bl21.metabolites.get_by_id("h_c")
+
+    #NADPH
+    nadph_c = bl21.metabolites.get_by_id("nadph_c")
+
+    #NADP+
+    nadp_c = bl21.metabolites.get_by_id("nadp_c")
+
+    #H20
+    h2o_c = bl21.metabolites.get_by_id("h2o_c")
+
+    #O2
+    o2_c = bl21.metabolites.get_by_id("o2_c")
+
+    ############### Reactions ###############
+
+    print("adding reactions")
+
+    #DLDH = uni.reactions.get_by_id("34HPLFM") #this reaction is for the mitochondiral compartment, written out for cytosol below
+
+    #HPP -> 4-hydroxyphenyllactate
+    DLDH = Reaction('DLDH')
+    DLDH.name = 'D-lactate dehydrogenase'
+    DLDH.subsystem = 'SAA module'
+    DLDH.lower_bound = -1000  # This is the default
+    DLDH.upper_bound = 1000  # This is the default
+    #3,4-Hydroxyphenyllactate + NAD+ <=> 3,4-Hydroxyphenylpyruvate + NADH + H+
+    DLDH.add_metabolites({
+        hpl34_c: -1.0,
+        nad_c: -1.0,
+        hpp34_c: 1.0,
+        nadh_c: 1.0,
+        h_c: 1.0
+    })
+    #DLDH.gene_reaction_rule = 'ldh' #synthetic ldh gene
+
+    #HPP -> 3,4-dihydroxyphenylpyruvate
+    HPPHD = Reaction('HPPHD')
+    HPPHD.name = '4-hydroxyphenylacetate 3-hydroxylase'
+    HPPHD.subsystem = 'SAA module'
+    HPPHD.lower_bound = -1000  # This is the default
+    HPPHD.upper_bound = 1000  # This is the default
+    #4-hydroxyphenylpyruvate + Oxygen + NADH + H+ <=> 3,4-dihydroxyphenylpyruvate + NAD+ + H2O
+    HPPHD.add_metabolites({
+        # hpp34_c: -1.0,
+        # o2_c: -1.0,
+        # nadh_c: -1.0,
+        # h_c: -1.0,
+        # dhpp34_c: 1.0,
+        # nad_c: 1.0,
+        # h2o_c: 1.0
+        hpp34_c: -1.0,
+        nadh_c: -1.0,
+        h_c: -1.0,
+        dhpp34_c: 1.0,
+        nad_c: 1.0,
+    })
+    #HPPHD.gene_reaction_rule = '(hpaB and hpaC)' #synthetic hpaBC gene
+    
+    #3,4-dihydroxyphenylpyruvate -> salvianic acid A
+    DHPPSA = Reaction('DHPPSA')
+    DHPPSA.name = '4-hydroxyphenyllactate:NAD+ oxidoreductase'
+    DHPPSA.subsystem = 'SAA module'
+    DHPPSA.lower_bound = -1000  # This is the default
+    DHPPSA.upper_bound = 1000  # This is the default
+    #3,4-Dihydroxyphenyllactate + NAD+ <=> 3,4-Dihydroxyphenylpyruvate + NADH + H+
+    DHPPSA.add_metabolites({
+        saa_c: 1.0,
+        nad_c: 1.0,
+        dhpp34_c: -1.0,
+        nadh_c: -1.0,
+        h_c: -1.0
+    })
+    #DHPPSA.gene_reaction_rule = 'ldh' #synthetic ldh gene
+
+    #4-hydroxyphenyllactate -> salvianic acid A
+    HPLSA = Reaction('HPLSA')
+    HPLSA.name = '(R)-3-(4-hydroxyphenyl)lactate:NADP+ oxidoreductase'
+    HPLSA.subsystem = 'SAA module'
+    HPLSA.lower_bound = -1000  # This is the default
+    HPLSA.upper_bound = 1000  # This is the default
+    #4-Hydroxyphenyllactate + Oxygen + NADH + H+ <=> 3,4-Dihydroxyphenylacetic acid + NAD+ + H2O
+    HPLSA.add_metabolites({
+        # hpl34_c: -1.0,
+        # o2_c: -1.0,
+        # nadh_c: -1.0,
+        # h_c: -1.0,
+        # saa_c: 1.0,
+        # nad_c: 1.0,
+        # h2o_c: 1.0
+        hpl34_c: -1.0,
+        nadh_c: -1.0,
+        h_c: -1.0,
+        saa_c: 1.0,
+        nad_c: 1.0,
+    })
+    #HPLSA.gene_reaction_rule = '(hpaB and hpaC)' #synthetic ldh gene
+
+    #SAA transport reaction
+    SAAt = Reaction('SAAt')
+    SAAt.name = 'caffa transport'
+    SAAt.subsystem = 'RA module'
+    SAAt.lower_bound = -1000  # This is the default
+    SAAt.upper_bound = 1000  # This is the default
+    SAAt.add_metabolites({
+        saa_c: -1.0,
+        saa_e: 1.0
+    })
+
+    # add reactions to model
+    SAL11.add_reactions([DLDH, HPPHD, DHPPSA, HPLSA, SAAt])
+
+    # exhange reactions for SA
+    SAL11.add_boundary(SAL11.metabolites.get_by_id("saa_e"), type="exchange")
+
+    # so that the cell cannot eat SA
+    SAL11.reactions.EX_saa_e.lower_bound = 0
+    
+    print("Testing model")
+
+    # run tests to see that the model functions as it should
+    assert no_energy_gen_cycles(SAL11), "Energy generation cycle detected"
+    assert production_possible(SAL11, "EX_saa_e"), "SAA production not possible"
+
+    # updtate the model ID
+    SAL11.id = 'SAL11'
+
+    # write model to file
+    file_path = ROOT_DIR / "community_modelling" / "GEMs" / "SAL11.xml"
+    print("Writing model to file as", str(file_path))
+    write_sbml_model(SAL11, file_path)
+    
 
 if __name__ == '__main__':
     freeze_support()
@@ -936,4 +1445,7 @@ if __name__ == '__main__':
     # sal9()
     # cal2()
     # mam1()
-    mra()
+    # mra()
+    # cal11()
+    # mam3()
+    sal11()
