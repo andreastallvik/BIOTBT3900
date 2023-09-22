50f2541e019d9c2d720fc062583dd0b8eef615ca
Andrea Stallvik <grimsdatter@gmail.com>
2023-09-22 15:11:11 +0200

    xylose/glucose triculture

--- community_modelling/RAsynthesis/KNOCK_OUTS.py
+++ community_modelling/RAsynthesis/KNOCK_OUTS.py
@@ -1,4 +1,21 @@
-"""Need a place to store the knock-outs that need be performed, both for cobrapy and reframed."""
+"""
+Lists of knock-outs that need be performed.
+
+for example in cobrapy:
+
+from cobra.manipulation import knock_out_model_genes
+import KNOCK_OUTS
+
+knock_out_model_genes(SAL11, KNOCK_OUTS.SAL11_KO)
+knock_out_model_genes(MAM3, KNOCK_OUTS.MAM3_KO)
+knock_out_model_genes(CAL11, KNOCK_OUTS.CAL11_KO)
+
+or in reframed:
+
+import KNOCK_OUTS
+
+
+"""
 
 CAL2_KO = ["b2599"]
 
