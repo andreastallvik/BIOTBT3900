084b7ca4ec0f04b5782094ed9a1c97f00c9169ac
Andrea Stallvik <grimsdatter@gmail.com>
2024-01-29 16:22:42 +0100

    testing growth coupling w/ knockouts

--- community_modelling/CBP_butanol/optknock_long_run.py
+++ community_modelling/CBP_butanol/optknock_long_run.py
@@ -1,12 +1,19 @@
 from cobra.io import read_sbml_model
 from utils.OptKnock import phonyOptKnock
+from utils import static_sim
 
 nj4 = read_sbml_model("GEMs/NJ4_curated.xml")
 
-optKnock = phonyOptKnock(nj4, "EX_ac_e", knockouts=1)
+medium = static_sim.get_specific_medium(nj4, {"EX_xyl__D_e": 10})
 
-optKnockdf = optKnock[optKnock['fva_min'] > 0]
+with nj4:
 
-print(len(optKnockdf))
+    nj4.medium = medium
 
-print(optKnockdf.sort_values('fva_min', ascending=False))
+    optKnock = phonyOptKnock(nj4, "EX_but_e", knockouts=1)
+
+    optKnockdf = optKnock[optKnock['fva_min'] > 0]
+
+    print(len(optKnockdf))
+
+    print(optKnockdf.sort_values('fva_min', ascending=False))
