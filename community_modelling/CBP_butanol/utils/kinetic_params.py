5e84868823b661cb8a1acb72d41b07463cf48e3e
Andrea Stallvik <grimsdatter@gmail.com>
2024-02-05 14:23:35 +0100

    wip on kinetic params

--- community_modelling/CBP_butanol/utils/kinetic_params.py
+++ community_modelling/CBP_butanol/utils/kinetic_params.py
@@ -1,13 +1,32 @@
 """Kinetic parameter dictionaries."""
 
+#TODO: replace each instance of "None" with correct values
+# CoAT params from that 1999 article (but v. approximate)
+
 KINETIC_PARAMS = {
     "M5": {
-        "km": {},
-        "vmax": {}
+        "km": {
+            "EX_xylan4_e": 5,
+            "EX_xylan8_e": 5,
+            "EX_xyl__D_e": 5,
+        },
+        "vmax": {
+            "EX_xylan4_e": 10,
+            "EX_xylan8_e": 10,
+            "EX_xyl__D_e": 10,
+        }
     },
     "NJ4": {
-        "km": {},
-        "vmax": {}
+        "km": {
+            "EX_ac_e": 60,
+            "EX_but_e": 161,
+            "EX_xyl__D_e": 5,
+        },
+        "vmax": {
+            "EX_ac_e": 10,
+            "EX_but_e": 10,
+            "EX_xyl__D_e": 10,
+        }
     },
 }
 
