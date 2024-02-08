9daef855e75e1b943a2010911f239242eaf6d1be
Andrea Stallvik <grimsdatter@gmail.com>
2024-02-08 14:34:54 +0100

    added ferrodoxin hydrogenase rx to nj4

--- community_modelling/CBP_butanol/utils/nj4_modification.py
+++ community_modelling/CBP_butanol/utils/nj4_modification.py
@@ -1,5 +1,6 @@
 """Preform all modifications to the out-of-the-box carveme reconstruction of NJ4 and save the modified model as NJ4_curated"""
 
+import cobra
 from cobra.io import read_sbml_model, write_sbml_model
 from config import ROOT_DIR
 
@@ -38,6 +39,18 @@ POR_syn = universal_model.reactions.get_by_id("POR_syn")
 # Ferredoxin:NAD+ reductase
 FNRR = universal_model.reactions.get_by_id("FNRR")
 
+# (FeFe)-hydrogenase, EC 1.12.1.4, KeGG rx: R09508
+HYDA = cobra.Reaction('HYDA')
+HYDA.add_metabolites({
+    nj4.metabolites.get_by_id("h2_c"): -2.0,
+    nj4.metabolites.get_by_id("nad_c"): -1.0,
+    nj4.metabolites.get_by_id("fdxo_2_2_c"): -2.0,
+    nj4.metabolites.get_by_id("h_c"): 5.0,
+    nj4.metabolites.get_by_id("nadh_c"): 1.0,
+    nj4.metabolites.get_by_id("fdxrd_c"): 2.0,
+})
+HYDA.bounds = (-1000, 1000)
+
 # make these reaction reversible like god intended
 # CoA- transferase reactions by enzyme 2.8.3.8
 nj4.reactions.get_by_id("BUTCT").bounds = (-1000, 1000) 
@@ -46,7 +59,7 @@ nj4.reactions.get_by_id("BUTCT2").bounds = (-1000, 1000)
 
 
 print("adding reactions...")
-nj4.add_reactions([BTOHt, BUTt, ADCi, ACEt, ACtr, ACOAD1, POR_syn, FNRR])
+nj4.add_reactions([BTOHt, BUTt, ADCi, ACEt, ACtr, ACOAD1, POR_syn, FNRR, HYDA])
 nj4.add_boundary(nj4.metabolites.get_by_id('btoh_e'), type='exchange', reaction_id='EX_btoh_e', lb=0, ub=1000)
 nj4.add_boundary(nj4.metabolites.get_by_id('acetone_e'), type='exchange', reaction_id='EX_acetone_e', lb=0, ub=1000)
 
